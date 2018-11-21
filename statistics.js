59b0c07bf95422c1ed10e7e77c81d27bdd36b920
Alejandro <alvarezherenasalejandro@gmail.com>
2018-11-21 15:41:45 +0100

    segunda parte del proyecto

---
+++ statistics.js
@@ -0,0 +1,12 @@
+var statistics = {
+
+    "NUMR": 0,
+    "NUMD": 0,
+    "NUMI": 0,
+    "votes_party_R_pct":0,
+    "votes_party_D_pct":0,
+    "member_m_Loyal_top_votes":[],
+    "member_l_Loyal_bottom_votes":[],
+    "member_m_engaged_attendace":[],
+    "member_m_engaged_attendace":[],
+}
